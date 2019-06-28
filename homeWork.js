0870d0ab0dfc66ea90baf74a1f35b2e25388b12f
taufanlai <taufan.lai@gmail.com>
2019-06-28 16:14:14 +0700

    Create homeWork.js

---
+++ homeWork.js
@@ -0,0 +1,101 @@
+const videoData = [
+    {
+        name: 'Miss Scarlet',
+        present: true,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Mrs. White',
+        present: false,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Reverend Green',
+        present: true,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Rusty',
+        present: false,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Colonel Mustard',
+        present: true,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Professor Plum',
+        present: true,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    }
+];
+
+let filterdata = e=>{
+    let result ;
+
+     result = e.filter(item=> item.present ==true);
+
+    return result;
+}
+
+
+let filterdata2 =e=>{
+
+    let result = [] ;
+
+      e.filter(item=> item.present ==true).map(t=>{
+        result.push(t.name);     
+    });
+
+    return result;
+}
+
+
+let dat = filterdata (videoData)
+console.log(dat)
+
+let dat2 = filterdata2(videoData)
+console.log(dat2)
